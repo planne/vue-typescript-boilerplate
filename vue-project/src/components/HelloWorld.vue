6a8110743e5756f849a59d6cc14c3d29673d978d
Hong Kwon <hongkwon.dev@gmail.com>
2023-01-03 18:31:28 +0100

    fix failing run lint

--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -9,8 +9,10 @@ defineProps<{
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a>
+      +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>
+      . What's next?
     </h3>
   </div>
 </template>
@@ -33,7 +35,6 @@ h3 {
 }
 
 @media (min-width: 1024px) {
-
   .greetings h1,
   .greetings h3 {
     text-align: left;
